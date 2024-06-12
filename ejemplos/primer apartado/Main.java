2be473a42ffeeb7813e480a802f4ab9fbdc79635
brean-rb <115301347+brean-rb@users.noreply.github.com>
2024-06-12 15:52:42 +0200

    añadiendo ejemplos 1, 2

---
+++ ejemplos/primer apartado/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String texto = "hola mundo";
+        int numeros_enteros = 2;
+
+        System.out.println(numeros_enteros);
+
+    }
+}
---
+++ ejemplos/segundo apartado/EstructurasCondicionales.java
@@ -0,0 +1,18 @@
+public class EstructurasCondicionales {
+    public static void main(String[] args) {
+        int numero =  ;
+
+        if (numero > 0) {
+            System.out.println("El número es positivo.");
+        } else if (numero < 0) {
+            System.out.println("El número es negativo.");
+        } else if (numero == 0) {
+            System.out.println("El número es cero.");
+        } else {
+            System.out.println("no se a cumplido la condicion ");
+        }
+
+
+
+    }
+}
